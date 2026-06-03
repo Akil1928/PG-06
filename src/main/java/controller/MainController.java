db180a5566a3bcb0ef885154bc3c5a45b0680bf6
Akil1928 <akilwatsonch1928@gmail.com>
2026-06-03 17:45:18 -0600

    a

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,4 +1,33 @@
 package controller;
 
-public class MainController {
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.TextAlignment;
+import javafx.util.Duration;
+import model.tree.*;
+
+import java.net.URL;
+import java.util.*;
+
+public class MainController implements Initializable {
+
+
+    @FXML
+    private Canvas btreeCanvas;
+    @FXML
+    private TabPane mainTabPane;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
